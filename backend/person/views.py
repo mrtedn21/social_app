7a0a6d7f21f718e31995c564a446730d26314b9a
mrtedn21 <bezgin.sasha06@gmail.com>
2022-09-06 11:33:11 +0700

    make PersonViewSet ReadOnly

--- backend/person/views.py
+++ backend/person/views.py
@@ -5,7 +5,7 @@ from person.models import Person
 from person.serializers import PersonSerializer
 
 
-class PersonViewSet(viewsets.ModelViewSet):
+class PersonViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
     permission_classes = (permissions.IsAuthenticated,)
