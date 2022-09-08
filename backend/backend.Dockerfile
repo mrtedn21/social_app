6cd47b2e5413d5ae4ad22813d15d75ba5c2c5493
mrtedn21 <bezgin.sasha06@gmail.com>
2022-09-08 11:42:18 +0700

    add connection to postgres

--- backend/backend.Dockerfile
+++ backend/backend.Dockerfile
@@ -2,12 +2,19 @@
 
 FROM python:3.10-slim-buster
 WORKDIR /app
+
 COPY poetry.lock poetry.lock
 COPY pyproject.toml pyproject.toml
+
 RUN python -m pip install --upgrade pip
 RUN python -m pip install --user poetry
+
+RUN apt-get update
+RUN apt-get install python3-dev libpq-dev -y
+
 ENV PATH="${PATH}:/root/.local/bin"
 RUN poetry config virtualenvs.create false
 RUN poetry install --no-root --no-interaction --no-ansi
+
 COPY . ./backend
 CMD ["poetry", "run", "python", "backend/manage.py", "runserver", "0:8000"]
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,11 +78,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'postgres',
+        'PORT': 5432,
+        'CONN_MAX_AGE': 600,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
