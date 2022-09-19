0fc10b94d230e2a145fe34f105fe01e4b03ce968
mrtedn21 <bezgin.sasha06@gmail.com>
2022-09-19 11:43:48 +0700

    add swagger support

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'person.apps.PersonConfig',
     'core.apps.CoreConfig',
 ]
