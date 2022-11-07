969ac4e2675d4054d8d1eddc3c72f2021aae0d02
mrtedn21 <bezgin.sasha06@gmail.com>
2022-11-07 13:26:03 +0700

    add person filter for photos list

--- backend/photo/views.py
+++ backend/photo/views.py
@@ -1,11 +1,13 @@
 from rest_framework import viewsets
 
+from photo.filters import PhotoFilter
 from photo.models import Photo
 from photo.serializers import PhotoSerializer, PhotoCreateSerializer
 
 
 class PhotoViewSet(viewsets.ModelViewSet):
     queryset = Photo.objects.all()
+    filterset_class = PhotoFilter
 
     def get_serializer_class(self):
         if self.action == 'create':
