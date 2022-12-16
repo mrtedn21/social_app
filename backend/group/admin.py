cb034734ac6938a297eabb369ce39943a646ed65
mrtedn21 <bezgin.sasha06@gmail.com>
2022-12-16 11:37:47 +0700

    add themes and followers to group model

--- backend/group/admin.py
+++ backend/group/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from group.models import Group
+from group.models import Group, GroupTheme
 
 
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug', 'short_description')
+    list_display = ('name', 'slug', 'short_description', 'theme')
+
+
+@admin.register(GroupTheme)
+class GroupThemeAdmin(admin.ModelAdmin):
+    pass
--- backend/group/models.py
+++ backend/group/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 from core.models import MultiImageMeta, null_and_blank
+from person.models import Person
+
+
+class GroupTheme(models.Model):
+    name = models.CharField(max_length=64)
+    slug = models.SlugField(max_length=64)
 
 
 class Group(models.Model, metaclass=MultiImageMeta):
@@ -9,4 +15,6 @@ class Group(models.Model, metaclass=MultiImageMeta):
     short_description = models.CharField(max_length=256, **null_and_blank)
     long_description = models.TextField(**null_and_blank)
     slug = models.SlugField(max_length=64)
+    theme = models.ForeignKey(GroupTheme, on_delete=models.CASCADE, null=True)
+    followers = models.ManyToManyField(Person, blank=True)
     created_date = models.DateField(auto_now_add=True)
