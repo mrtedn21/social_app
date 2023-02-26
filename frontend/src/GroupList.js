b3ee0a93c8a720efcfd669bfabeeb3b67e4e58d7
mrtedn21 <bezgin.sasha06@gmail.com>
2023-02-26 16:59:01 +0700

    add group group filters to html

--- frontend/src/GroupList.js
+++ frontend/src/GroupList.js
@@ -24,14 +24,18 @@ class GroupList extends React.Component {
         const groups = this.state.groups.map(group => (
             <div className="card mb-3">
                 <div className="row g-0">
-                    <div className="col-md-2">
-                        <img src={group.avatar_thumbnail} className="img-fluid rounded-start" alt="..." width="100px"/>
+                    <div className="col-md-3">
+                        <img src={group.avatar_thumbnail} className="img-fluid rounded" alt="..." width="100px"/>
                     </div>
-                    <div className="col-md-10">
-                        <div className="card-body">
-                            <h5 className="card-title">{group.name}</h5>
-                            <p className="card-text">{group.short_description}</p>
+                    <div className="col-md-6">
+                        <div className="card-body" style={{'paddingRight': 0,'paddingLeft': 0}}>
+                            <a href="#" style={{marginBottom: '3px', color: 'black'}}>{group.name}</a>
+                            <p style={{marginTop: '0', marginBottom: '0'}}>{group.theme_name}</p>
+                            <p style={{marginTop: '0', marginBottom: '0'}}>{group.followers_count} followers</p>
                         </div>
+                    </div>
+                    <div className="col-md-3">
+                        <input type="button" value="Following" className="btn btn-primary" style={{marginTop: '10px', marginRight: '10px', float: 'right'}}/>
                     </div>
                 </div>
             </div>
@@ -41,10 +45,25 @@ class GroupList extends React.Component {
             <Container>
                 <div className="row" style={{marginTop: '15px'}}>
                     <div className="col-3"></div>
-                    <div className="col-6">
+                    <div className="col-5">
+                        <input type="text" value={this.state.first_name} className="form-control" placeholder="Search"
+                               name="search" onChange={this.inputHandle} style={{margin: '15px 0'}}/>
                         {groups}
                     </div>
-                    <div className="col-3"></div>
+                    <div className="col-2" style={{marginTop: '15px'}}>
+                        <div>Theme filters</div>
+
+                        <div className="form-check">
+                            <input className="form-check-input" type="radio" name="flexRadioDefault"/>
+                            <label className="form-check-label">Default radio</label>
+                        </div>
+                        <div class="form-check">
+                            <input className="form-check-input" type="radio" name="flexRadioDefault"/>
+                            <label className="form-check-label">Default checked radio</label>
+                        </div>
+
+                    </div>
+                    <div className="col-2"></div>
                 </div>
             </Container>
         )
