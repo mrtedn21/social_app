01de5c56aed3d96fdebf0bcff7405016b78b95fd
mrtedn21 <bezgin.sasha06@gmail.com>
2023-03-25 18:38:17 +0700

    add admin for group photos

--- backend/photo/admin.py
+++ backend/photo/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from photo.models import PersonPhoto
+from photo.models import GroupPhoto, PersonPhoto
 
 admin.site.register(PersonPhoto)
+admin.site.register(GroupPhoto)
--- backend/post/admin.py
+++ backend/post/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
 from post.models import GroupPost, PersonPost
 
-
-@admin.register(PersonPost)
-class PersonPostAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(GroupPost)
-class GroupPostAdmin(admin.ModelAdmin):
-    pass
+admin.site.register(PersonPost)
+admin.site.register(GroupPost)
