59513b78763511fd4e7e97066edbf9acd3798498
mrtedn21 <bezgin.sasha06@gmail.com>
2023-04-09 13:41:23 +0700

    frontend: make reloadnig page after song upload

--- frontend/src/person/PersonMusic.js
+++ frontend/src/person/PersonMusic.js
@@ -73,7 +73,7 @@ class PersonMusic extends React.Component {
                 'Authorization': 'Token ' + token,
             },
         })
-            //.then(response => window.location.reload())
+            .then(response => window.location.reload())
     }
 
     render() {
